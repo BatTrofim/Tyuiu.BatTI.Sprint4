11b8afcf8adcb06d757b8ecea31346da6414928b
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-01 21:12:41 +0500

    Task0

--- Tyuiu.BatTI.Sprint4.Task0.V29.Lib/DataService.cs
+++ Tyuiu.BatTI.Sprint4.Task0.V29.Lib/DataService.cs
@@ -1,6 +1,5 @@
-﻿using System.Globalization;
-using tyuiu.cources.programming.interfaces.Sprint4;
-    
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
 namespace Tyuiu.BatTI.Sprint4.Task0.V29.Lib
 {
     public class DataService : ISprint4Task0V29

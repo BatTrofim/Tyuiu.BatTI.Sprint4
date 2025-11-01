7526eec295120f87acc3bd9ee39add42a6d0887f
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-01 19:48:37 +0500

    Add project files.

---
+++ Tyuiu.BatTI.Sprint4.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.BatTI.Sprint4.Task0.V29.Lib
+{
+    public class DataService : ISprint4Task0V29
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int nums = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    nums = nums + array[i];
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint4.Task0.V29/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.BatTI.Sprint4.Task0.V29.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #4                                                                *");
+Console.WriteLine("* Тема: Одномерные массивы                                                 *");
+Console.WriteLine("* Задание #0                                                               *");
+Console.WriteLine("* Вариант #29                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* подсчитать сумму нечетных элементов массива                              *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+
+int[] numsArray = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+int res = ds.GetSumOddArrEl(numsArray);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Результат = " + ds.GetSumOddArrEl(numsArray));
+Console.ReadKey();

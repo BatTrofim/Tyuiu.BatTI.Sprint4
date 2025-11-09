fff963d7f8e3a57f279482c6e25d3806cab6262d
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-09 16:34:52 +0500

    Task2

---
+++ Tyuiu.BatTI.Sprint4.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BatTI.Sprint4.Task2.V22.Lib;
+
+namespace Tyuiu.BatTI.Sprint4.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 8, 6, 4, 9, 5, 8, 7, 8, 4, 7, 6, 8 };
+            int res = ds.Calculate(numsArray);
+            int wait = 2205;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

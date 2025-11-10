852be821dd9b99c6d466168acd85678468d194e0
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-10 18:04:02 +0500

    Task6

---
+++ Tyuiu.BatTI.Sprint4.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BatTI.Sprint4.Task6.V16.Lib;
+
+namespace Tyuiu.BatTI.Sprint4.Task6.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var car = new string[] { "Автомобиль", "Велосипед", "Самолет", "Поезд", "Трамвай", "Троллейбус", "Метро" };
+            string [] res = ds.Calculate(car);
+            string[] wait = { "Самолет", "Трамвай" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint4.Task6.V16/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.BatTI.Sprint4.Task6.V16.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #4                                                                *");
+Console.WriteLine("* Тема: Класс Array                                                        *");
+Console.WriteLine("* Задание #6                                                               *");
+Console.WriteLine("* Вариант #16                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* вывести все элементы, длина которых равна 7                              *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+var car = new string[] { "Автомобиль", "Велосипед", "Самолет", "Поезд", "Трамвай", "Троллейбус", "Метро" };
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < car.Length - 1; i++)
+{
+    Console.Write(car[i] + " ");
+   
+}
+var res = ds.Calculate(car);
+string result = string.Join(", ", res);
+Console.Write("Элементы, длина которых 7:\n " + result);
+
+Console.ReadLine();
